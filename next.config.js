ba17ae074a7905c672172e94fc88d46116909d50
Dancan Kimani <dkimani@fh.org>
2024-11-13 00:18:48 +0300

    ignore lint

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ["."],
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: ["images.clerk.dev", "www.gravatar.com"],
